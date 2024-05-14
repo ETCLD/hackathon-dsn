e79eb44240713debe62d4b6cf16d3788a583c0c7
Kévin Aupée <kevin.aupee@etcld.fr>
2024-05-14 17:41:09 +0200

    integrate api to get metabase iframe

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from typing import Union
+from datetime import datetime, timedelta
+import jwt
+
+METABASE_URL = "http://10.0.0.58:3000"
+METABASE_SECRET_KEY = "9b4c5bcf5ac34b459f656da44588028c36d3d6cf1a8d3b419676fcbeee17bb58"
+METABASE_DURATION = 600
 
 app = FastAPI()
 
@@ -13,12 +18,30 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-# Tests for later
 class MetabaseFilter(BaseModel):
-    departement: Union[str, None] = None
-    siren: Union[str, None] = None
-    year: Union[int, None] = None
+    dashboard_id: int
+    filters: dict
+
+
+def make_metabase_iframe_response(filter: MetabaseFilter):
+    """Generate metabase iframe"""
+    expiration = datetime.now() + timedelta(seconds=METABASE_DURATION)
+
+    payload = {
+        "resource": {
+            "dashboard": filter.dashboard_id
+        },
+        "params": filter.filters,
+        "exp": round(datetime.timestamp(expiration))
+    }
+
+    token = jwt.encode(payload, METABASE_SECRET_KEY, algorithm="HS256")
+    base_url = METABASE_URL + "/embed/dashboard/"
+
+    return {
+        "iframe_url": base_url + token + "#bordered=false&titled=false",
+        "url_expiration": expiration
+    }
 
 
 @app.get("/")
@@ -27,4 +50,4 @@ def root():
 
 @app.post("/dashboard/")
 def dashboard(filter: MetabaseFilter):
-    return filter
+    return make_metabase_iframe_response(filter)

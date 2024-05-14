3e4aafcab2deb77cbccac8fb13cc872635ccb872
Kévin Aupée <kevin.aupee@etcld.fr>
2024-05-14 15:50:23 +0200

    init: add vue & fastapi

---
+++ hackathon-dsn/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Metabase from './components/Metabase.vue'
+</script>
+
+<template>
+  <div>
+    Vue
+  </div>
+  <Metabase />
+</template>
+
+<style scoped>
+</style>
---
+++ hackathon-dsn/src/components/Metabase.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        Metabase
+    </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+    data() {
+        return {
+        };
+    },
+
+    methods: {},
+    computed: {},
+    mounted() {
+        try {
+            axios.post("http://localhost:8000/dashboard/", {"year": null, "departement": null, "siren": null})
+                .then((response) => {
+                    console.log(response.data);
+                });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+</script>
+
+<style scoped></style>
